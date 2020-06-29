9a604d65e7d9e7800ae68e064631dd92059c564f
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 22:24:32 +0530

    Update header.component.jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,10 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const { isCartHidden } = useContext(GlobalContext);
   return (
-    
+    <div className="header">
+      <div className={`options`}>
+        
+    </div>
   );
 };
 
