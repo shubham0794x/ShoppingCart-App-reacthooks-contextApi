ac14c8682ef4ddd3b3d7fb7c3b340c14f792712c
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 22:26:25 +0530

    Update product-collection.component.jsx

--- src/components/product-collection/product-collection.component.jsx
+++ src/components/product-collection/product-collection.component.jsx
@@ -6,7 +6,11 @@ import Grid from "@material-ui/core/Grid";
 import "./product-collection.styles.scss";
 
 const ProductCollection = () => (
-  
+  <div className="collections-overview">
+    <Grid container spacing={2}>
+     
+    </Grid>
+  </div>
 );
 
 export default ProductCollection;
