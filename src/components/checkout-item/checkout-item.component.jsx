6117d4c9f811953ba5b92161f983cd4eeec0592a
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 22:14:44 +0530

    Update checkout-item.component.jsx

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,18 +4,25 @@ const CheckoutItem = ({ cartItem }) => {
     const { imageUrl, name, quantity, price } = cartItem;
     const { addItem, deleteItem, clearItem } = useContext(GlobalContext);
     return (
-        <div className="checkout-item">
-            <div className="image-container">
-                <img alt="item" src={imageUrl} />
-            </div>
-            <span className="name">{name}</span>
-            <span className="quantity">
-                <div className="arrow" onClick={() => deleteItem(cartItem.id)}>
-                    &#10094;
+      <div className="checkout-item">
+        <div className="image-container">
+          <img alt="item" src={imageUrl} />
         </div>
-                
+        <span className="name">{name}</span>
+        <span className="quantity">
+          <div className="arrow" onClick={() => deleteItem(cartItem.id)}>
+            &#10094;
+          </div>
+          <span className="value">{quantity}</span>
+          <div className="arrow" onClick={() => addItem(cartItem)}>
+            &#10095;
+          </div>
+        </span>
+        <span className="price">{price}</span>
+        <div className="remove-button" onClick={() => clearItem(cartItem.id)}>
+          &#10005;
+        </div>
       </div>
-        </div>
     );
 };
 
