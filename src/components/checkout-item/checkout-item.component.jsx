9fddbf27c52908838dcdb60c93051611250bcf7b
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 22:14:20 +0530

    Update checkout-item.component.jsx

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 const CheckoutItem = ({ cartItem }) => {
-  const { imageUrl, name, quantity, price } = cartItem;
-  const { addItem, deleteItem, clearItem } = useContext(GlobalContext);
-  return (
-            
-  );
+    const { imageUrl, name, quantity, price } = cartItem;
+    const { addItem, deleteItem, clearItem } = useContext(GlobalContext);
+    return (
+        <div className="checkout-item">
+            <div className="image-container">
+                <img alt="item" src={imageUrl} />
+            </div>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={() => deleteItem(cartItem.id)}>
+                    &#10094;
+        </div>
+                
+      </div>
+        </div>
+    );
 };
 
 export default CheckoutItem;
