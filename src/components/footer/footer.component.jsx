4d427a67563e049f05ba1766e994deaace6789c2
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 22:19:39 +0530

    Update footer.component.jsx

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,6 +4,17 @@ import "./footer.styles.scss";
 
 const Footer = () => {
   return (
-    
+    <div className="footer">
+      <div className="content">
+        <div className="options">
+          <div className="heading">About</div>
+          <About />
+        </div>
+        <div className="options">
+          <div className="heading">Quick Links</div>
+          <QuickLinks />
+        </div>
+
+        
   );
 };
