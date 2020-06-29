bd5e8e11c2dcff152dd6e60d68c05614bed079af
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 22:24:13 +0530

    Update header.component.jsx

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+  const { isCartHidden } = useContext(GlobalContext);
+  return (
+    
+  );
+};
+
+export default Header;
