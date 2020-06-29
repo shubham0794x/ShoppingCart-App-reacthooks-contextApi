1e71301ead5fc9403dc885c332a9c2c8cde73847
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 22:26:06 +0530

    Update product-collection.component.jsx

---
+++ src/components/product-collection/product-collection.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductItem from "../product-item/product-item.component";
+
+import Grid from "@material-ui/core/Grid";
+
+import "./product-collection.styles.scss";
+
+const ProductCollection = () => (
+  
+);
+
+export default ProductCollection;
